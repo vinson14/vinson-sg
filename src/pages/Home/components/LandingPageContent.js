61ba2bbac51a06fbb15643b217f38994c817cdec
Vinson <vinson1493@gmail.com>
2021-02-18 17:14:25 +0800

    Added line break to landing page content

--- src/pages/Home/components/LandingPageContent.js
+++ src/pages/Home/components/LandingPageContent.js
@@ -4,7 +4,7 @@ const LandingPageContent = () => {
     return (
         <div className="col-12 col-md-5 p-5 translucent-bg">
             <h1 className="pri-font-light">
-                Hi there! I am{" "}
+                Hi there! <br />I am{" "}
                 <span className="pri-font-bold green-font">Vinson</span>
             </h1>
             <Typewriter />
