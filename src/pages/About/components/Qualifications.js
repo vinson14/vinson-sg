a17d2f8c5c61f47f74cccbc72653263647330228
Vinson <vinson1493@gmail.com>
2021-02-20 02:30:17 +0800

    Edited qualifications data

--- src/pages/About/components/Qualifications.js
+++ src/pages/About/components/Qualifications.js
@@ -22,7 +22,7 @@ const Qualification = ({ name, institution }) => {
     return (
         <div className="qualification pri-font col-12 col-md-5 p-3 m-3 text-center">
             <h6>{parse(name)}</h6>
-            <h6 className="gold-font">{parse(institution)}</h6>
+            <h6 className="green-font">{parse(institution)}</h6>
         </div>
     );
 };
