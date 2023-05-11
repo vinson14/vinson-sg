0827c3a45f04ddd0c9d2dc8ad24c7669f08c9798
Vinson Ong <vinson.ong@omnihr.co>
2023-05-11 09:48:38 +0800

    Added cupid song

--- src/pages/Home/components/LandingPageContent.js
+++ src/pages/Home/components/LandingPageContent.js
@@ -1,11 +1,16 @@
+import { useEffect } from "react";
 import Typewriter from "./Typewriter";
 
 const LandingPageContent = () => {
+    useEffect(() => {
+        const audio = new Audio("cupid.mp3");
+        audio.play();
+    }, []);
+
     return (
         <div className="col-12 col-md-5 p-5 translucent-bg">
             <h1 className="pri-font-light">
-                Hi there! <br />I am{" "}
-                <span className="pri-font-bold green-font">Vinson</span>
+                Hi there! <br />I am <span className="pri-font-bold green-font">Vinson</span>
             </h1>
             <Typewriter />
         </div>
